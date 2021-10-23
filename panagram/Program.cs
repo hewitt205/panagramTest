0d7ba9d68523ed0f5541d3f97b09a8dc89ce110a
hewitt205 <hewitt205@yahoo.com>
2021-10-22 19:35:10 -0500

    Add project files.

---
+++ panagram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace panagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPangram("The quick brown fox jumps over the lazy dog"));
+        }
+        public static bool IsPangram(string str)
+        {
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+            int count = 0;
+            string input = str.ToLower();
+            foreach (char c in alpha)
+            {
+               foreach (char b in input)
+                {
+                    if (c == b)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            if (count == 26)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
